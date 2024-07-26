a5cbd80b7de91bfe3697cf22967d7419077f722e
Rahulpr365 <sparksmediaofficial@gmail.com>
2024-07-26 13:40:17 +0530

    Create function_with_no_argument_no return value.c

---
+++ function_with_no_argument_no return value.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+void odd_even(int n) {
+    if (n % 2 == 0) {
+        printf("The number is even\n");
+    } else {
+        printf("The number is odd\n");
+    }
+}
+
+void positive_negative(int n) {
+    if (n > 0) {
+        printf("The number is positive\n");
+    } else if (n < 0) {
+        printf("The number is negative\n");
+    } else {
+        printf("The number is zero\n");
+    }
+}
+
+int main() {
+    int n;
+
+    printf("Enter a number: ");
+    scanf("%d", &n);
+
+    odd_even(n);
+    positive_negative(n);
+
+    return 0;
+}
